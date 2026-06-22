490b9c03ea53a0358f55439739f6cb7d2de38d22
Daryl Stark <daryl@dstark.nl>
2026-06-22 13:05:25 +0200

    feat: Non-interactive updates.
    Fixes #58

--- src/update/001-update-system.sh
+++ src/update/001-update-system.sh
@@ -11,6 +11,6 @@ require_root
 print_header "Updating Generic Dev Container / System"
 
 # System packages
-sudo apt update && \
-    apt full-upgrade -y --no-install-recommends
+sudo apt-get update && \
+    apt-get full-upgrade -y -o Dpkg::Options::="--force-confold"
 
